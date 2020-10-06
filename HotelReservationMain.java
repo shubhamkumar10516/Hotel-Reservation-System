0672463059b36dee44bc5a47e14447c65fdc044f
shubham <shubhamkumar10516@gmail.com>
2020-10-06 20:47:08 +0530

    [Shubham]ADDED: Welcome message master file added

---
+++ HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgeLabz.HotelReservation;
+
+public class HotelReservationMain {
+
+	public static void printWelcomeMessage() {
+		System.out.println("*******Welcome to Hotel Reservation System*******");
+	}
+	
+}
